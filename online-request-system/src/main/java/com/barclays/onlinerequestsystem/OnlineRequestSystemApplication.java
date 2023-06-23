5a75dc9d4e9f44fb57d93966251cffdf1dd27763
Jagadeeswara Patnam <jagadeeswarapatnam94@gmail.com>
2023-06-23 10:55:22 +0530

    project setup by jagan java developer

---
+++ online-request-system/src/main/java/com/barclays/onlinerequestsystem/OnlineRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.onlinerequestsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRequestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRequestSystemApplication.class, args);
+	}
+
+}
---
+++ online-request-system/src/test/java/com/barclays/onlinerequestsystem/OnlineRequestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.onlinerequestsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRequestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
